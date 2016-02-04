3d6d901dc8cdf20a9bdc6464eff2f6d73622b109
guyupeng <yupeng@sonation.net>
2016-02-04 13:34:07 -0500

    Initial Commit

---
+++ SymMark/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SymMark
+//
+//  Created by Yupeng Gu on 2/4/16.
+//  Copyright © 2016 Yupeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
